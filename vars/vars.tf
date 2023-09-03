34851b4a92af56bab5fb45aeb0623993fd6d2626
sairam <sairm21@gmail.com>
2023-09-03 18:52:24 +0530

    vars demo

--- vars/vars.tf
+++ vars/vars.tf
@@ -18,3 +18,35 @@ output "dem" {
 output "variable_in_strig" {
   value = "value of demo is ${var.demo}"
 }
+
+variable "insurance" {
+  default = "general insurance"
+}
+
+variable "general_insurance" {
+  default = ["car","bike","health"]
+}
+
+variable "insurance_details" {
+  default = {
+    bike = {
+      registration = "mh075824"
+      mobile = 7508509501
+      previous_poloicy = "expired"
+    }
+    health = {
+      adults = 2
+      children = 2
+      pincode = 560259
+      mobile = 7508509501
+    }
+  }
+}
+
+output "gen_ins_types" {
+  value = var.general_insurance[2]
+}
+
+output "insurance_details" {
+  value = var.insurance_details["health"]
+}
