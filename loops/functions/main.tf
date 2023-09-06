b2ddf0d186a8572959d9a3e4e6b7fd1dd55d41d4
sairam <sairm21@gmail.com>
2023-09-06 21:20:28 +0530

    length function

--- loops/functions/main.tf
+++ loops/functions/main.tf
@@ -5,3 +5,15 @@ variable "upper" {
 output "upper_conversion" {
   value = upper(var.upper)
 }
+
+variable "length" {
+  default = ["string1", "string2", "string3"]
+}
+
+output "length_output" {
+  value = length(var.length)
+}
+
+output "string_length" {
+  value = length(var.upper)
+}
