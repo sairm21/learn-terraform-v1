053b0bf73182c1be9d36a23d570a441820b31baa
sairam <sairm21@gmail.com>
2023-09-06 21:35:27 +0530

    lookup function

--- loops/functions/main.tf
+++ loops/functions/main.tf
@@ -17,3 +17,27 @@ output "length_output" {
 output "string_length" {
   value = length(var.upper)
 }
+
+variable "courses" {
+  default = {
+    terraform = {
+      name = "terraform with any cloud"
+      topics = ["terraform", "aws basics", "azure adminstrator"]
+    }
+    devops = {
+      name = "devops"
+      topics = ["aws", "terraform", "ansible", "containers"]
+    }
+    docker = {
+      name = "docker"
+    }
+  }
+}
+
+output "devops_topics" {
+  value = var.courses["devops"]["topics"]
+}
+
+output "docker_topics" {
+  value = lookup(var.courses, "docker", null)
+}
