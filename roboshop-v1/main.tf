51162f01f036ff030130451fe0ef9c0a8e9d2dbe
sairam <sairm21@gmail.com>
2023-09-07 09:02:30 +0530

    roboshop instances creation using for each

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,54 +1,21 @@
-module "Frontend" {
+module "roboshop_instances" {
   source = "./ec2"
-  name = "Frontend"
+  name = each.key
+  for_each = var.instances
 }
 
-module "MongoDB" {
-  source = "./ec2"
-  name = "MongoDB"
-}
-
-module "Catalogue" {
-  source = "./ec2"
-  name = "Catalogue"
-}
-
-module "Redis" {
-  source = "./ec2"
-  name = "Redis"
-}
-
-module "User" {
-  source = "./ec2"
-  name = "User"
-}
-
-module "Cart" {
-  source = "./ec2"
-  name = "Cart"
-}
-
-module "MySQL" {
-  source = "./ec2"
-  name = "MySQL"
-}
-
-module "Shipping" {
-  source = "./ec2"
-  name = "Shipping"
-}
-
-module "RabbitMQ" {
-  source = "./ec2"
-  name = "RabbitMQ"
-}
-
-module "Payment" {
-  source = "./ec2"
-  name = "Payment"
-}
-
-module "Dispatch" {
-  source = "./ec2"
-  name = "Dispatch"
+variable "instances" {
+  default = {
+    Frontend ={}
+    MongoDB ={}
+    Catalogue ={}
+    Redis ={}
+    User ={}
+    Cart ={}
+    MySQL ={}
+    Shipping ={}
+    RabbitMQ ={}
+    Payment ={}
+    Dispatch ={}
+  }
 }
